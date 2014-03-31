34ad430b9c490e33a8a828c6048548e73311cecd
SergeyKnyazev <sergey.n.knyazev@gmail.com>
2014-03-31 14:18:26 +0400

    wip

--- scripts/main.py
+++ scripts/main.py
@@ -15,7 +15,12 @@ HOME_DIR = '/opt/bio'
 ROSETTA_DIR = HOME_DIR + '/rosetta3.4'
 BASE_DIR = ROSETTA_DIR + '/Antibody'
 DATABASE = BASE_DIR + '/database'
+
+# Ig database
 INFO = BASE_DIR + '/info'
+
+CDR1_REGEX = 'C[A-Z]{13,15}W[IVFYAMLN][RKQVNC][QKHELR]'
+CDR2_REGEX = 'C[A-Z]{5,27}WG[A-Z][GRDS]'
 
 __author__ = 'Sergey Knyazev'
 
@@ -89,22 +94,22 @@ class HeavyChain(Chain):
 
     def _annotate(self):
         #find CDR1
-        p = re.compile('C[A-Z]{13,15}W[IVFYAMLN][RKQVNC][QKHELR]')
+        p = re.compile(CDR1_REGEX)
         r = p.search(self.query)
 
         if r is None:
-            print("CDR1 is not found in: " + self.query)
+            print('CDR1 is not found in: ' + self.query)
             exit(1)
 
         self._cdr1_begin = r.start() + 9
         self._cdr1_length = r.end() - self._cdr1_begin - 4
 
         #find CDR3
-        p = re.compile('C[A-Z]{5,27}WG[A-Z][GRDS]')
+        p = re.compile(CDR2_REGEX)
         r = p.search(self.query, self._cdr1_begin + self._cdr1_length)
 
         if r is None:
-            print("CDR3 is not found in: " + self.query)
+            print('CDR3 is not found in: ' + self.query)
             exit(1)
 
         self._cdr3_begin = r.start() + 3
@@ -114,7 +119,7 @@ class HeavyChain(Chain):
         self._cdr2_length = self._cdr3_begin - self._cdr2_begin - 33
 
         if self._cdr2_length < 4:
-            print("CDR2 is not found in: " + self.query)
+            print('CDR2 is not found in: ' + self.query)
             exit(1)
 
 
@@ -142,20 +147,81 @@ class BlastAnalyseParameters():
         self.max_length_deviation = 0
 
 
-class BlastAnalysis():
+class BlastAnalyser():
+    CDR_DATA_BASE = '/Cdrdatabase_all'
+    OUTLIER_LIST = '/outlierlist'
+    LIGHT_HEAVY_LENGTH_FILE = '/lightheavylengthfile'
+
     input_fasta = ''
+
+    length = dict()
+    Seq = dict()
+    resol = dict()
+    Source = dict()
+    vltype = dict()
+    outlier_result = dict()
+
 
     #TODO:
     @staticmethod
     def init(input_fasta, info_path):
-        BlastAnalysis.input_fasta = input_fasta
-        BlastAnalysis.__init_database_info(info_path)
-        pass
+        BlastAnalyser.input_fasta = input_fasta
+        BlastAnalyser.__init_database_info(info_path)
 
-    #TODO:
     @staticmethod
     def __init_database_info(info_path):
-        pass
+        with open(info_path + CDR_DATA_BASE, 'r') as f:
+            for line in f:
+                filename, resol, lenl1, lenl2, lenl3, lenh1, lenh2, lenh3, \
+                seql1, seql2,seql3, seqh1, seqh2, seqh3, source, date, \
+                vltype, frag = line.split()
+
+                BlastAnalyser.resol[filename] = resol
+
+                #3h0t is the only member in the database to have two insertions
+                #in the light framework, viz. 66A and 66B
+                if filename == 'pdb3h0t_chothia.pdb':
+                    BlastAnalyser.length[filename + "lfr"] = 64
+                else:
+                    BlastAnalyser.length[filename + "lfr"] = 62
+
+                BlastAnalyser.length[filename + "hfr"] = 67
+                BlastAnalyser.length[filename + "lbeta"] = 22
+                BlastAnalyser.length[filename + "hbeta"] = 23
+                BlastAnalyser.length[filename + "l1"] = lenl1
+                BlastAnalyser.length[filename + "l2"] = lenl2
+                BlastAnalyser.length[filename + "l3"] = lenl3
+                BlastAnalyser.length[filename + "h1"] = lenh1
+                BlastAnalyser.length[filename + "h2"] = lenh2
+                BlastAnalyser.length[filename + "h3"] = lenh3
+
+                BlastAnalyser.Seq[filename + "l1"] = seql1
+                BlastAnalyser.Seq[filename + "l2"] = seql2
+                BlastAnalyser.Seq[filename + "l3"] = seql3
+                BlastAnalyser.Seq[filename + "h1"] = seqh1
+                BlastAnalyser.Seq[filename + "h2"] = seqh2
+                BlastAnalyser.Seq[filename + "h3"] = seqh3
+
+                BlastAnalyser.Source[filename] = source
+                BlastAnalyser.vltype[filename] = vltype
+
+        try:
+            with open (info_path + BlastAnalyser.LIGHT_HEAVY_LENGTH_FILE, 'r' ) as f:
+                for line in f:
+                    filename , chaintype , length = line.split()
+                    BlastAnalyser.length[filename + chaintype] = length
+        except EnvironmentError:
+            print('Exiting. File ' + info_path + BlastAnalyser.LIGHT_HEAVY_LENGTH_FILE + ' not found\n')
+            exit()
+
+        try:
+            with open (info_path + BlastAnalyser.OUTLIER_LIST, 'r' ) as f:
+                for line in f:
+                    argument, result = line.split()
+                    BlastAnalyser.outlier_result[argument] = result
+        except EnvironmentError:
+            print('Exiting. File ' + info_path + BlastAnalyser.OUTLIER_LIST + ' not found\n')
+            exit()
 
     @staticmethod
     def __get_fasta_filename(fasta, database):
@@ -186,7 +252,7 @@ class BlastAnalysis():
 
     @staticmethod
     # TODO:
-    def __analyse_blast_output(self, blastfile, analyse_parameters):
+    def __analyse_blast_output(blastfile, analyse_parameters):
         with open(blastfile, 'r') as f:
             blast_reader = ParseBlastTable.BlastTableReader(f)
             for entrie in blast_reader.__next__().__dict__['entries']:
@@ -200,7 +266,7 @@ class BlastAnalysis():
 
     # TODO:
     def get_best_homolog(self):
-        fasta = self.__get_fasta_filename(BlastAnalysis.input_fasta, self.blast_alignment_parameters.database_name)
+        fasta = self.__get_fasta_filename(BlastAnalyser.input_fasta, self.blast_alignment_parameters.database_name)
         self.__write_fasta(fasta, self.sequence)
         blast_file = self.__blast_fragment(fasta, self.blast_alignment_parameters)
         return self.__analyse_blast_output(blast_file, self.blast_analysis_parameters)
@@ -209,37 +275,37 @@ class BlastAnalysis():
 def find_fragments_homologs(chain, args):
     blast_fragments = dict()
 
-    BlastAnalysis.init(args.input_fasta, INFO)
+    BlastAnalyser.init(args.input_fasta, INFO)
     blast_alignment_parameters = BlastAlingmentParameters(args.database)
     blast_analyse_parameters = BlastAnalyseParameters()
 
     blast_alignment_parameters.e_value = 0.00001
-    blast_alignment_parameters.matrix = "BLOSUM62"
+    blast_alignment_parameters.matrix = 'BLOSUM62'
     blast_alignment_parameters.word_size = 0
-    blast_alignment_parameters.database_name = "database.hfr"
+    blast_alignment_parameters.database_name = 'database.hfr'
 
-    blast_fragments["fr"] = BlastAnalysis(copy.deepcopy(blast_alignment_parameters),
+    blast_fragments['fr'] = BlastAnalyser(copy.deepcopy(blast_alignment_parameters),
                                           copy.deepcopy(blast_analyse_parameters),
                                           chain.fr1() + chain.fr2() + chain.fr3())
 
     blast_alignment_parameters.e_value = 2000
-    blast_alignment_parameters.matrix = "PAM30"
+    blast_alignment_parameters.matrix = 'PAM30'
     blast_alignment_parameters.word_size = 2
-    blast_alignment_parameters.database_name = "database.h1"
+    blast_alignment_parameters.database_name = 'database.h1'
 
-    blast_fragments["h1"] = BlastAnalysis(copy.deepcopy(blast_alignment_parameters),
+    blast_fragments['h1'] = BlastAnalyser(copy.deepcopy(blast_alignment_parameters),
                                           copy.deepcopy(blast_analyse_parameters),
                                           chain.cdr1())
 
-    blast_alignment_parameters.database_name = "database.h2"
+    blast_alignment_parameters.database_name = 'database.h2'
 
-    blast_fragments["h2"] = BlastAnalysis(copy.deepcopy(blast_alignment_parameters),
+    blast_fragments['h2'] = BlastAnalyser(copy.deepcopy(blast_alignment_parameters),
                                           copy.deepcopy(blast_analyse_parameters),
                                           chain.cdr2())
 
-    blast_alignment_parameters.database_name = "database.h3"
+    blast_alignment_parameters.database_name = 'database.h3'
 
-    blast_fragments["h3"] = BlastAnalysis(copy.deepcopy(blast_alignment_parameters),
+    blast_fragments['h3'] = BlastAnalyser(copy.deepcopy(blast_alignment_parameters),
                                           copy.deepcopy(blast_analyse_parameters),
                                           chain.cdr3())
 
@@ -262,10 +328,10 @@ def parse_args():
 
 def main():
     args = parse_args()
-    heavy_chain = HeavyChain(str(SeqIO.parse(args.input_fasta, "fasta").__next__().seq))
+    heavy_chain = HeavyChain(str(SeqIO.parse(args.input_fasta, 'fasta').__next__().seq))
     fragments_homologs = find_fragments_homologs(heavy_chain, args)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
